cb35baa3aedd80a968cc637420a1b640c85d2a8a
BAHOHCEHC <bahohcehc@gmail.com>
2025-12-24 02:17:10 +0200

    first action
    add bse setup

--- src/app/pages/admin/enemies-editor/enemies-editor.ts
+++ src/app/pages/admin/enemies-editor/enemies-editor.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { multiFactor } from 'firebase/auth';
 
 @Component({
   standalone: true,
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './enemies-editor.scss',
 })
 export class EnemiesEditor {
-
+  openEnemiesUniversalModal(entity: string): void {
+    console.log('realization');
+  }
 }
