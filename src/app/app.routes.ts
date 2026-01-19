5144a437837f995d0424faa5c7da4a6eb98ac2e9
BAHOHCEHC <bahohcehc@gmail.com>
2026-01-19 11:17:50 +0200

    lazy load routes
    lazy load routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,27 +12,76 @@ import { SeasonsDetails } from './pages/season-details/season-details';
 import { TaskTrackerAdmin } from './pages/admin/task-tracker-admin/task-tracker-admin';
 import { TaskTracker } from './pages/task-tracker/task-tracker';
 
-
 // app.routes.ts
 export const routes: Routes = [
-  { path: '', component: LineupSimulator },
-  { path: 'season', component: SeasonsDetails },
-  { path: 'characters', component: YourCharacters },
-  { path: 'enemies', component: EnemyList },
-  { path: 'task-tracker', component: TaskTracker },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/lineup-simulator/lineup-simulator').then((c) => c.LineupSimulator),
+  },
+  {
+    path: 'season',
+    loadComponent: () =>
+      import('./pages/season-details/season-details').then((c) => c.SeasonsDetails),
+  },
+  {
+    path: 'characters',
+    loadComponent: () =>
+      import('./pages/your-characters/your-characters').then((c) => c.YourCharacters),
+  },
+  {
+    path: 'enemies',
+    loadComponent: () => import('./pages/enemy-list/enemy-list').then((c) => c.EnemyList),
+  },
+  {
+    path: 'task-tracker',
+    loadComponent: () => import('./pages/task-tracker/task-tracker').then((c) => c.TaskTracker),
+  },
 
   {
     path: 'admin',
     canActivate: [adminGuard],
-    component: AdminDashboard,
+    loadComponent: () =>
+      import('./pages/admin/admin-dashboard/admin-dashboard').then((c) => c.AdminDashboard),
     children: [
-      { path: '', redirectTo: 'characters', pathMatch: 'full' },
-
-      { path: 'characters', component: CharacterListEditor },
-      { path: 'enemies', component: EnemyEditor },
-      { path: 'modes', component: ActAndModesEditor },
-      { path: 'seasons', component: SeasonsEditorComponent },
-      { path: 'ttracker', component: TaskTrackerAdmin },
-    ]
-  }
+      {
+        path: '',
+        redirectTo: 'characters',
+        pathMatch: 'full',
+      },
+      {
+        path: 'characters',
+        loadComponent: () =>
+          import('./pages/admin/character-list-editor/character-list-editor').then(
+            (c) => c.CharacterListEditor,
+          ),
+      },
+      {
+        path: 'enemies',
+        loadComponent: () =>
+          import('./pages/admin/enemy-editor/enemy-editor').then((c) => c.EnemyEditor),
+      },
+      {
+        path: 'modes',
+        loadComponent: () =>
+          import('./pages/admin/modes-editor/act-and-modes-editor').then(
+            (c) => c.ActAndModesEditor,
+          ),
+      },
+      {
+        path: 'seasons',
+        loadComponent: () =>
+          import('./pages/admin/seasons-editor/seasons-editor').then(
+            (c) => c.SeasonsEditorComponent,
+          ),
+      },
+      {
+        path: 'ttracker',
+        loadComponent: () =>
+          import('./pages/admin/task-tracker-admin/task-tracker-admin').then(
+            (c) => c.TaskTrackerAdmin,
+          ),
+      },
+    ],
+  },
 ];
